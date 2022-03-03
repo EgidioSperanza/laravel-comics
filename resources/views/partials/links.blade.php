0d863c9c9d38aad56f0634adf904b645f4ce9079
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-03-03 20:46:54 +0100

    fix footer's links section

--- resources/views/partials/links.blade.php
+++ resources/views/partials/links.blade.php
@@ -1,51 +1,57 @@
 <div class="footer-link">
     <div class="footer-link-inner">
       <div class="link-box">
-        <div class="col">
-          <h3>DC Comics</h3>
-          <ul>
-            <li>Characters</li>
-            <li>Comics</li>
-            <li>Movies</li>
-            <li>TV</li>
-            <li>Games</li>
-            <li>Videos</li>
-            <li>News</li>
-          </ul>
-
-          <h3>Shop</h3>
-          <ul>
-            <li>Shop DC</li>
-            <li>Shop DC Collectibles</li>
-          </ul>
+        <div class="link-box-col">
+          <div class="col">
+            <h3>DC Comics</h3>
+            <ul>
+              <li>Characters</li>
+              <li>Comics</li>
+              <li>Movies</li>
+              <li>TV</li>
+              <li>Games</li>
+              <li>Videos</li>
+              <li>News</li>
+            </ul>
+  
+            <h3>Shop</h3>
+            <ul>
+              <li>Shop DC</li>
+              <li>Shop DC Collectibles</li>
+            </ul>
+          </div>
+          <div class="col">
+            <h3>DC</h3>
+            <ul>
+              <li>Terms Of Use</li>
+              <li>Privacy Policy (New)</li>
+              <li>Ad Choises</li>
+              <li>Advertising</li>
+              <li>Jobs</li>
+              <li>Subscriptions</li>
+              <li>Talent Workshops</li>
+              <li>CPSC Certificates</li>
+              <li>Ratings</li>
+              <li>Shop Help</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+          <div class="col">
+            <h3>Sites</h3>
+            <ul>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+            </ul>
+          </div>
         </div>
-        <div class="col">
-          <h3>DC</h3>
-          <ul>
-            <li>Terms Of Use</li>
-            <li>Privacy Policy (New)</li>
-            <li>Ad Choises</li>
-            <li>Advertising</li>
-            <li>Jobs</li>
-            <li>Subscriptions</li>
-            <li>Talent Workshops</li>
-            <li>CPSC Certificates</li>
-            <li>Ratings</li>
-            <li>Shop Help</li>
-            <li>Contact Us</li>
-          </ul>
-        </div>
-        <div class="col">
-          <h3>Sites</h3>
-          <ul>
-              <li>DC</li>
-              <li>MAD Magazine</li>
-              <li>DC Kids</li>
-              <li>DC Universe</li>
-              <li>DC Power Visa</li>
-          </ul>
-        </div>
+          <p class="copy">All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="#">noted here</a>. All rights reserved.</p>
+          <p><a href="#">Cookies Settings</a></p>
       </div>
       <div class="footer-logo"></div>
+    </div>
+    <div>
     </div>
   </div>
